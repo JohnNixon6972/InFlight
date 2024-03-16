5930a91d09bc35900e3b6f22c6a49d40c7130a6c
JohnNixon6972 <johnbrightnixon@gmail.com>
2024-03-16 16:35:06 +0000

    worked

--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -57,7 +57,7 @@ const Dashboard = () => {
       <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
         <Widget
           icon={<MdBarChart className="h-7 w-7" />}
-          title={"Total Entires"}
+          title={"Total Entries"}
           subtitle={data ? data.total_entries.toLocaleString() + " records" : "Loading..."}
         />
         <Widget
